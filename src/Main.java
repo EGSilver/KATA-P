5ee5742d68d165f0f997b2cb38321797785293ec
EGSilver <dean.vervaeck@outlook.com>
2023-05-05 11:29:24 +0200

    ValidatePassword Kata

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.List;
+
+class Main {
+        public static void main(String[] args) {
+            String password = "Some+";
+            List<String> errorMessages = ValidatePassword.checkPassword(password);
+            for (String message : errorMessages) {
+                System.out.println(message);
+            }
+        }
     }
-}
---
+++ src/ValidatePassword.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidatePassword {
+
+    public static List<String> checkPassword(String password) {
+        List<String> errorMessages = new ArrayList<>();
+
+        if (password.length() < 8) {
+            errorMessages.add("Password must be at least 8 characters");
+        }
+
+        Pattern digitPattern = Pattern.compile("(\\d)");
+        Matcher digitMatcher = digitPattern.matcher(password);
+        int digitCount = 0;
+        while (digitMatcher.find()) {
+            digitCount++;
+        }
+        if (digitCount < 2) {
+            errorMessages.add("The password must contain at least 2 numbers");
+        }
+
+        Pattern capitalLetterPattern = Pattern.compile("([A-Z])");
+        Matcher capitalLetterMatcher = capitalLetterPattern.matcher(password);
+        if (!capitalLetterMatcher.find()) {
+            errorMessages.add("Password must contain at least one capital letter");
+        }
+
+        Pattern specialCharacterPattern = Pattern.compile("([!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?])");
+        Matcher specialCharacterMatcher = specialCharacterPattern.matcher(password);
+        if (!specialCharacterMatcher.find()) {
+            errorMessages.add("Password must contain at least one special character");
+        }
+
+        return errorMessages;
+    }
+}
